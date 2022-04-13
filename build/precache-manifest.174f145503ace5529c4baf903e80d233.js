14efa56d02c4e9ef5d2ca24832a9ecea57f37285
trannguyenvu3482 <21088201.vu@student.iuh.edu.vn>
2022-04-13 20:09:37 +0700

    reupload build

---
+++ build/precache-manifest.174f145503ace5529c4baf903e80d233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b13bf194eda5b9572ac06bc49ad55f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66ef2c6b50dafff69f89",
+    "url": "/static/css/main.289c1fa7.chunk.css"
+  },
+  {
+    "revision": "9aff709ffce6dd2dd0a7",
+    "url": "/static/js/2.5b374e73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5b374e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ef2c6b50dafff69f89",
+    "url": "/static/js/main.9112f697.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
